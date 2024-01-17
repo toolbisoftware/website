4a7c584faa64bcf5f3b605fd168e66615f9814bb
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-17 17:15:43 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
