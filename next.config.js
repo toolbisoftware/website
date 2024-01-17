1cd7fd12c85cef61fd880035010a2f6c64b2f77d
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-17 17:15:25 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
