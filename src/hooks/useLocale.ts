2576d14e97d8add11d24cb33b90e5f3d33eafec8
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-27 12:47:49 +0100

    Create useLocale.ts

---
+++ src/hooks/useLocale.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+import { useLocale as use } from "@/providers/Locale";
+
+export function useLocale() {
+  return use();
+}
