33d8df742e537533648239a40cf375ebc6ff1fa4
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-27 12:51:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+import { getLocale } from "@/i18n/server";
+import Layout from "@/layouts/Root";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./main.css";
+import Providers from "./providers";
+
+const afacad = localFont({
+  src: [
+    { path: "../public/assets/fonts/Afacad.woff2", style: "normal" },
+    { path: "../public/assets/fonts/Afacad-Italic.woff2", style: "italic" }
+  ],
+  variable: "--font-primary"
+});
 
 export const metadata: Metadata = {};
 
@@ -11,9 +23,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const locale = getLocale();
+
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang={locale}
+      className={`${afacad.variable}`}
+    >
+      <body>
+        <Providers locale={locale}>
+          <Layout>{children}</Layout>
+        </Providers>
+      </body>
     </html>
   );
 }
