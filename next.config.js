5cdb0524ccd5df2370cf6659e7ff043cd43f4697
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-29 18:12:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 // Check the README file in the project root for more information.
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"]
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  }
+};
 
 module.exports = nextConfig;
